c659e1eb3fc68c6113b8ad1f6fca6fbdaa786b2d
Ronak Gupta <84411925+Ronyano83@users.noreply.github.com>
2022-09-18 00:24:30 +0530

    Time: 120 ms (77.52%), Space: 46.5 MB (97.73%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int l = nums.size();
+        sort(nums.begin(), nums.end());
+        
+        for(int i=0; i<l-1;i++){
+        if(nums[i]==nums[i+1]) return true;
+            
+        }
+        
+      return false;  
+    }
+};
