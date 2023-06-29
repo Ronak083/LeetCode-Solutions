503d7201a0da94d543a882258c1a173107295f01
Ronak Gupta <ronakgupta083@gmail.com>
2023-06-30 00:31:02 +0530

    Time: 1 ms (100%), Space: 55.9 MB (31.73%) - LeetHub

---
+++ 0932-monotonic-array/0932-monotonic-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        if(nums[0]<nums[nums.length-1]) {
+            for(int i = 0;i<nums.length-1;i++){
+                if(nums[i] <= nums[i+1]) ;
+                else return false;
+            }
+        } else {
+            for(int i = 0;i<nums.length-1;i++){
+                if(nums[i] >= nums[i+1]) ;
+                else return false;
+            }
+        }
+        return true;
+    }
+}
