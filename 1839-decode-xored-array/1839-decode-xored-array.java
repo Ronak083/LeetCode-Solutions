ff047fc9f40d5bd66670d52a6b8f753b98fc52b5
Ronak Gupta <ronakgupta083@gmail.com>
2023-06-29 19:03:01 +0530

    Time: 2 ms (6.39%), Space: 45 MB (64.86%) - LeetHub

--- 1839-decode-xored-array/1839-decode-xored-array.java
+++ 1839-decode-xored-array/1839-decode-xored-array.java
@@ -1,11 +1,10 @@
 class Solution {
     public int[] decode(int[] encoded, int first) {
         int[] arr = new int[encoded.length + 1];
-        int j = 0;
-        arr[j++] = first;
+        arr[0] = first;
         for(int i =0;i<encoded.length;i++){
             int x = encoded[i]^arr[i];
-            arr[j++] = x;
+            arr[i+1] = x;
         }
         return arr;
 
