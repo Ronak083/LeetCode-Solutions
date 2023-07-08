71bf8c8df3f3d2a686858c5b3c6c8c79a92c96b2
Ronak Gupta <ronakgupta083@gmail.com>
2023-07-09 01:24:47 +0530

    Time: 1 ms (35.24%), Space: 41.5 MB (43.01%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,26 +1,33 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m-1;
-        int j = n-1;
-        int k = m+n-1;
-        while(k>=0){
-            if(i>=0 && j>=0){
-                if(nums1[i]<nums2[j]){
-                    nums1[k] = nums2[j];
-                    k--;
-                    j--;
-                } else{
-                    nums1[k] = nums1[i];
-                    k--;
-                    i--;
-                }
-            } else if(j>=0){
-                nums1[k] = nums2[j];
-                k--;
-                j--;
-            } else{
-                break;
-            }
+        //int i = m-1;
+        //int j = n-1;
+        //int k = m+n-1;
+        //while(k>=0){
+        //    if(i>=0 && j>=0){
+        //        if(nums1[i]<nums2[j]){
+        //            nums1[k] = nums2[j];
+        //            k--;
+        //            j--;
+        //        } else{
+        //            nums1[k] = nums1[i];
+        //            k--;
+        //            i--;
+        //        }
+        //    } else if(j>=0){
+        //        nums1[k] = nums2[j];
+        //        k--;
+        //        j--;
+        //    } else{
+        //        break;
+        //    }
+        //}
+
+        int j =0;
+        for(int i = m;j<n;i++){
+            nums1[i] = nums2[j];
+            j++;
         }
+        Arrays.sort(nums1);
     }
 }
